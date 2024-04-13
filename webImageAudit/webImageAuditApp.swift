572502e4addbcbf2f00d5638ed50ab82f7442675
Matthew Thomas Stevens <matthew.stevens@matthewstevens.me>
2024-04-12 20:40:48 -0500

    Initial Commit

---
+++ webImageAudit/webImageAuditApp.swift
@@ -0,0 +1,17 @@
+//
+//  webImageAuditApp.swift
+//  webImageAudit
+//
+//  Created by Matthew Stevens on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct webImageAuditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
